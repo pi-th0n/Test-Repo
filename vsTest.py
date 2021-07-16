cd41292b4c03b9f270e7e48e5b533c82f98bb502
Wiltered <85147481+pi-th0n@users.noreply.github.com>
2021-07-16 09:42:57 -0700

    Made some changes to vsTest.py

--- vsTest.py
+++ vsTest.py
@@ -2,3 +2,7 @@ import requests
 
 r = requests.get("https://pi-th0n.github.io/")
 print(r.status_code)
+
+print(
+    "This is to test GitHub stuff in VSCode! If you come across this, there is nothing interesting here."
+)
